1c69fb666fe1d486a77eab85d475641ed8cce151
klkvsky <b.kulikovsky@icloud.com>
2022-10-25 01:38:35 +0500

    changes are made

--- src/components/feedbackComponent.jsx
+++ src/components/feedbackComponent.jsx
@@ -31,6 +31,13 @@ export default function FeedbackComponent(prop) {
   const [submited, isSubmited] = useState(false);
   const location = useLocation();
   //
+  // const phoneInput = document.querySelector("input[type=tel]");
+  setTimeout(() => {
+    const phoneInput = document.querySelector(".react-tel-input .form-control");
+  phoneInput?.setAttribute("type", "tel");
+  phoneInput?.setAttribute("id", "phone");
+  phoneInput?.setAttribute("name", "phone");
+  }, 1000);
   return (
     <div>
       {submited && (
@@ -87,16 +94,14 @@ export default function FeedbackComponent(prop) {
         </div>
       )}
       <form
-        className="mt-[50px] w-[300px] flex flex-col gap-[20px] mx-auto lg:w-[362px]"
-        id={`${location.pathname}-form`}
-        onSubmit={(e) => {
-          sumbitFunc(e);
-        }}
         method="POST"
         action="https://sobakipavlova23493.activehosted.com/proc.php"
+        id="_form_4_"
+        className="_form _form_4 _inline-form  _dark"
+        noValidate
       >
-        <input type="hidden" name="u" value="6" />
-        <input type="hidden" name="f" value="6" />
+        <input type="hidden" name="u" value="4" />
+        <input type="hidden" name="f" value="4" />
         <input type="hidden" name="s" />
         <input type="hidden" name="c" value="0" />
         <input type="hidden" name="m" value="0" />
@@ -105,23 +110,114 @@ export default function FeedbackComponent(prop) {
         <input
           type="hidden"
           name="or"
-          value="c38a2a3f2d23f367052f8c8b4a0572d1"
+          value="aff7854179c8dca119fc24ec49221b70"
         />
-        {/* <input
+        <div className="_form-content mt-[50px] w-[300px] flex flex-col gap-[20px] mx-auto lg:w-[362px] ">
+          <div className="_form_element _x54994482 _full_width ">
+            <label htmlFor="fullname" className="_form-label"></label>
+            <div className="_field-wrapper">
+              <input
+                type="text"
+                id="fullname"
+                name="fullname"
+                placeholder="  Имя"
+                required
+                className="border-[1px] w-full border-[#9e9e9e] rounded-[5px] text-[16px] h-[40px] px-[20px] lg:h-[50px]"
+              />
+            </div>
+          </div>
+          <div className="_form_element _x16231362 _full_width ">
+            <label htmlFor="email" className="_form-label"></label>
+            <div className="_field-wrapper">
+              <input
+                type="text"
+                id="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                name="email"
+                placeholder=" e-mail"
+                required
+                className="border-[1px] w-full border-[#9e9e9e] rounded-[5px] text-[16px] h-[40px] px-[20px] lg:h-[50px]"
+              />
+            </div>
+          </div>
+          <div className="_form_element _x92297975 _full_width ">
+            <label htmlFor="phone" className="_form-label"></label>
+            <div className="_field-wrapper">
+              <ReactPhoneInput
+                country={"ru"}
+                className="rounded-[5px] overflow-hidden lg:bg-red-500 lg:overflow-visible lg:rounded-[15px]"
+                required
+              />
+            </div>
+          </div>
+          {prop.disable != 4 && (
+            <select
+              name="Selectbox"
+              style={{
+                color: "#858585",
+                border: "1px solid #9e9e9e",
+                backgroundColor: "#ffffff",
+                borderRadius: "5px",
+                fontSize: "16px",
+                fontWeight: "400",
+                height: window.innerWidth < 1024 ? "40px" : "50px",
+              }}
+              className="px-[15px] bg-white shadow-none appearance-none"
+              id={`${location.pathname}-select`}
+              required
+            >
+              <option value="">Что вас беспокоит?</option>
+              <option value="Взаимоотношения с родителями ">
+                Взаимоотношения с родителями{" "}
+              </option>
+              <option value="Отношения">Отношения</option>
+              <option value="Самореализация ">Самореализация </option>
+              <option value="Самооценка ">Самооценка </option>
+              <option value="Тревожность ">Тревожность </option>
+              <option value="Бессонница">Бессонница</option>
+              <option value="Депрессия ">Депрессия </option>
+              <option value="Помогаем">Помогаем</option>
+              <option value="Зависимость ">Зависимость </option>
+              <option value="Созависимость ">Созависимость </option>
+              <option value="Не понимаю, но нужна помощь">
+                Не понимаю, но нужна помощь
+              </option>
+            </select>
+          )}
+          <div className="_button-wrapper _full_width">
+            <button
+              id="_form_4_submit"
+              type="submit"
+              className="bg-[#5d6541] text-white text-[14px] font-semibold h-[50px] px-[15px] rounded-full w-full uppercase _submit"
+            >
+              ОСТАВИТЬ ЗАПРОС
+            </button>
+          </div>
+          <div className="_clear-element"></div>
+        </div>
+        <div className="_form-thank-you" style={{ display: "none" }}></div>
+      </form>
+
+      {/* <form
+        className="mt-[50px] w-[300px] flex flex-col gap-[20px] mx-auto lg:w-[362px]"
+        // id={`${location.pathname}-form`}
+        id="_form_6_"
+        onSubmit={(e) => {
+          sumbitFunc(e);
+        }}
+        method="POST"
+        action="https://sobakipavlova23493.activehosted.com/proc.php"
+        noValidate
+      >
+        <input
           type="text"
           name="fullname"
           placeholder="имя"
           className="border-[1px] w-full border-[#9e9e9e] rounded-[5px] text-[16px] h-[40px] px-[20px] lg:h-[50px]"
           id={`${location.pathname}-name`}
           required
-        /> */}
-        <input
-          type="text"
-          id="fullname"
-          name="fullname"
-          placeholder="Type your name"
         />
-        {/* <input
+        <input
           type="mail"
           name="mail"
           placeholder="e-mail"
@@ -129,15 +225,8 @@ export default function FeedbackComponent(prop) {
           id={`${location.pathname}-mail`}
           required
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-        /> */}
-        <input
-          type="text"
-          id="email"
-          name="email"
-          placeholder="Type your email"
-          required
         />
-        {/* <ReactPhoneInput
+        <ReactPhoneInput
           country={"ru"}
           className="rounded-[5px] overflow-hidden lg:bg-red-500 lg:overflow-visible lg:rounded-[15px]"
           id={`${location.pathname}-phone`}
@@ -176,7 +265,7 @@ export default function FeedbackComponent(prop) {
               Не понимаю, но нужна помощь
             </option>
           </select>
-        )} */}
+        )}
 
         <button
           className="bg-[#5d6541] text-white text-[14px] font-semibold h-[50px] px-[15px] roun
@@ -186,7 +275,7 @@ export default function FeedbackComponent(prop) {
         >
           Оставить запрос
         </button>
-      </form>
+      </form> */}
     </div>
   );
 }

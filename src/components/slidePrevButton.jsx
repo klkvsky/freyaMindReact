57575d39d391c6dc39ee6f2bee7714783828988c
Symegu <89914120+Symegu@users.noreply.github.com>
2022-10-29 00:27:54 +0300

    slider btns added

--- src/components/slidePrevButton.jsx
+++ src/components/slidePrevButton.jsx
@@ -5,6 +5,8 @@ export default function SlidePrevButton() {
   const swiper = useSwiper();
   console.log(swiper)
   return (
-      <button onClick={() => swiper.slidePrev()}>Slide to the prev slide</button>
+      <button onClick={() => swiper.slidePrev()}><svg width="20" height="38" viewBox="0 0 20 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 1L1 19L19 37" stroke="black"/>
+      </svg></button>
   );
 }

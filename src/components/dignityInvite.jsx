d4770973834d65f42e42441b04144f9aeefb7bed
a0ne <mako666777@gmail.com>
2022-10-28 18:20:24 +0300

    res

--- src/components/dignityInvite.jsx
+++ src/components/dignityInvite.jsx
@@ -5,6 +5,20 @@ export default function DignityInvite() {
     return (
         <section>
             <p>Invite check</p>
+            <h2>Групповая терапия за 1 €</h2>
+            <p>Вы получите инструменты для быстрой адаптации на новом месте за 3 сессии по 90 минут.</p>
+            <a href="https://t.me/freya_mind_bot">
+                <button>ЗАПИСАТЬСЯ</button>
+            </a>
+            <p>До начала встречи еще есть время, пора записаться</p>
+            <img style={{position: 'absolute',
+                left: '52.15%',
+                right: '-1.94%',
+                top: '93.34%',
+                bottom: '0.06%',
+                width: '717px',
+                height: '350px',}} src="../../icons/Layer%202.png"/>
         </section>
+
     )
 }

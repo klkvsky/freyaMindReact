87c864a95f84a91b8e821fc6fa1ee642b1095977
Symegu <89914120+Symegu@users.noreply.github.com>
2022-10-28 19:22:47 +0300

    speakers upd

--- src/components/dignitySpeakers.jsx
+++ src/components/dignitySpeakers.jsx
@@ -10,16 +10,31 @@ export default function DignitySpeakers() {
                         <div className="speaker__container">
                             <img src="/src/assets/Website Assets/Dignity/Speakers/Asset 1@144x 1.png" alt="therapist"/>
                             <div className="speaker__overlay">
-                                <div className="speaker__education">
-                                    <p className="education">Образование</p>
-                                    <p>Московский городской психолого-педагогический университет, факультет клинической психологии, нейро и патопсихологии</p>
-                                </div>
-                                <div className="speaker__quote"></div>
+                                <img className="speaker__education" src="/src/assets/Website Assets/Dignity/Speakers/Asset 6@144x 1 (1).png" alt="info"  />
                             </div>
                         </div>
+                        <div className="speaker__name">Екатерина Быстрова</div>
                     </li>
+                    <li className="speaker">
+                        <div className="speaker__container">
+                            <img src="/src/assets/Website Assets/Dignity/Speakers/Asset 3@144x 1.png" alt="therapist"/>
+                            <div className="speaker__overlay">
+                                <img className="speaker__education" src="/src/assets/Website Assets/Dignity/Speakers/Asset 6@144x 1.png" alt="info" />
+                            </div>
+                        </div>
+                        <div className="speaker__name">Мария Домбровская</div>
+                    </li>
+                    <li className="speaker">
+                        <div className="speaker__container">
+                            <img src="/src/assets/Website Assets/Dignity/Speakers/Asset 2@144x 1.png" alt="therapist"/>
+                            <div className="speaker__overlay">
+                                <img className="speaker__education" src="/src/assets/Website Assets/Dignity/Speakers/Asset 6@144x 1 (2).png" alt="info" />
+                            </div>
+                        </div>
+                        <div className="speaker__name">Наталья Гладских</div>
+                    </li>
+                    <h1 className="speakers__heading">Спикеры</h1>
                 </ul>
-                <h1 className="speakers__heading"></h1>
             </div>
             <img className="speakers__svg" src="/src/assets/Website Assets/Dignity/Speakers/Group 613.svg"/>
         </section>

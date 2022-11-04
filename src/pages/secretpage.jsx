1097956f81ee08686a55bd65b9cb930088353ac5
a0ne <mako666777@gmail.com>
2022-11-05 01:19:26 +0300

    added btn

--- src/pages/secretpage.jsx
+++ src/pages/secretpage.jsx
@@ -1,9 +1,10 @@
-import {Link} from "react-router-dom";
+
 
 export default function SecretPage() {
     return(
-        <section className="flex flex-col mx-auto items-center pt-[150px]">
-            <a href="https://freyabystro.simplybook.me/v2/#book" target="_blank"><p className="w-[300px] h-[80px] bg-green-500 text-center rounded-md pt-[30px]">CREATE AN APPOINTMENT</p></a>
+        <section className="flex flex-col mx-auto items-center justify-center pt-[324px]">
+            <h2 className="book-heading">Schedule a session</h2>
+            <a href="https://freyabystro.simplybook.me/v2/#book" target="_blank"><p className="book-btn">BOOK</p></a>
         </section>
     )
 }

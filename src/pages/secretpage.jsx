db49a74b1aff7342f673493951a42c0d843464a1
a0ne <mako666777@gmail.com>
2022-11-05 00:50:35 +0300

    added button

--- src/pages/secretpage.jsx
+++ src/pages/secretpage.jsx
@@ -1,7 +1,9 @@
+import {Link} from "react-router-dom";
+
 export default function SecretPage() {
     return(
-        <section>
-
+        <section className="flex flex-col mx-auto items-center pt-[150px]">
+            <a href="https://freyabystro.simplybook.me/v2/#book" target="_blank"><p className="w-[300px] h-[80px] bg-green-500 text-center rounded-md pt-[30px]">CREATE AN APPOINTMENT</p></a>
         </section>
     )
 }

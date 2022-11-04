b163c8c3ba52cae4354406992a629fd0acbe216f
a0ne <mako666777@gmail.com>
2022-11-05 01:35:03 +0300

    mob button

--- src/pages/secretpage.jsx
+++ src/pages/secretpage.jsx
@@ -2,7 +2,7 @@
 
 export default function SecretPage() {
     return(
-        <section className="flex flex-col mx-auto items-center justify-center pt-[324px]">
+        <section className="secret-page">
             <h2 className="book-heading">Schedule a session</h2>
             <a href="https://freyabystro.simplybook.me/v2/#book" target="_blank"><p className="book-btn">BOOK</p></a>
         </section>

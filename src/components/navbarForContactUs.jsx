6a25a80d88715362d9bc01a66579e6d51e781473
Symegu <89914120+Symegu@users.noreply.github.com>
2022-11-06 11:57:39 +0300

    fixed routes and links + fixed psychologists for pilar + hid some styles

--- src/components/navbarForContactUs.jsx
+++ src/components/navbarForContactUs.jsx
@@ -52,7 +52,7 @@ export default function Navbar() {
             >
               Цена
             </NavLink>
-            <li>
+            {/* <li>
               <details>
                 <summary className="text-center flex flex-row items-end justify-center gap-[8px]">
                   Помогаем{" "}
@@ -80,7 +80,10 @@ export default function Navbar() {
                   <div className="w-96 h-px bg-neutral-500 opacity-50"></div>
                 </ul>
               </details>
-            </li>
+            </li> */}
+            <NavLink to="/1-Euro-Group-Therapy">
+              ГРУППОВАЯ ТЕРАПИЯ
+            </NavLink>
             <NavLink
               className={(navData) => (navData.isActive ? "font-bold" : "")}
               to="/livestream"
